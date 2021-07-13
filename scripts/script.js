8bd47df7baa37d8dfc13746409179bb010cd83c2
Lanrayy <lanre.sodeinde1@gmail.com>
2021-07-13 13:20:56 +0100

    Scripts added and button background colour started

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,90 @@
-let num = document.querySelector(".tip-button")
+// TIP AMOUNT RESULTS
+let tipAmountPerPerson = document.querySelector(".tip-amount-result-display");
+let totalAmountPerPerson = document.querySelector(".total-result-display");
+let billInput = document.querySelector(".bill-input");
+let numOfPeopleInput = document.querySelector(".num-of-people");
+let billAmount = 0;
+let tipAmount = 1;
+let numOfPeople = 1;
 
-num.addEventListener("click",function(){
-    
+// BILL INPUT
+billInput.addEventListener("input", function(){
+    console.log(billInput.value);
+    billAmount = billInput.value;
+    calculateResults();
 });
 
 
-let biillInput = document.querySelector(.bill)
-
-function submit(num){
+//TIP AMOUNT
+function submit(element, num){
     console.log(num)
+    tipAmount = num/100;
+    console.log(tipAmount);
+    calculateResults();
+    let buttonName = element.classList[0];
+    setColour(buttonName);
+    let list = element.classList
+    console.log(list);
+
 }
+
+// NUMBER OF PEOPLE
+numOfPeopleInput.addEventListener("input", function(){
+    numOfPeople = numOfPeopleInput.value;
+    calculateResults()
+});
+
+
+function calculateResults(){
+    console.log(tipAmount);
+    console.log(numOfPeopleInput.value);
+    // Tip Amount
+    tipAmountPerPerson.innerHTML = billAmount*tipAmount/numOfPeople;
+};
+
+// BUTTONS BACKGROUND
+let fivePercent = document.querySelector(".fivePercent");
+let tenPercent = document.querySelector(".tenPercent");
+let fifteenPercent = document.querySelector(".fifteenPercent");
+let twentyFivePercent = document.querySelector(".twentyFivePercent");
+let fiftyPercent = document.querySelector(".fiftyPercent");
+
+
+let setColour = function(buttonName){
+    console.log(buttonName);
+
+    //change existing colour to green
+    fivePercent.style.backgroundColor = "hsl(183, 100%, 15%)"
+    tenPercent.style.backgroundColor = "hsl(183, 100%, 15%)"
+    fifteenPercent.style.backgroundColor = "hsl(183, 100%, 15%)"
+    twentyFivePercent.style.backgroundColor = "hsl(183, 100%, 15%)"
+    fiftyPercent.style.backgroundColor = "hsl(183, 100%, 15%)"
+
+
+}
+
+//CALCULATIONS
+/*
+Tip amount per person = bill amount * percentage / number of people
+
+*/
+
+
+
+
+
+
+
+// tipAmountResult.innerHTML = `$`+
+
+
+
+
+
+
+
+
+
+
 
 
